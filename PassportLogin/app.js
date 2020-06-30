21cac877312c0e344f304e272df492a1085f3438
Revanth-M <revanthkumar.makkena@gmail.com>
2020-06-30 10:05:59 -0500

    Finished Passport login using EJS

--- PassportLogin/app.js
+++ PassportLogin/app.js
@@ -4,8 +4,11 @@ const expressLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
 const flash = require("connect-flash");
 const session = require("express-session");
+const passport = require("passport");
 const port = process.env.npm_package_config_port || 4001;
 
+//Passport config
+require("./config/passport")(passport);
 //DB config
 const db = require("./config/keys").MongoUri;
 
@@ -31,6 +34,10 @@ app.use(
   })
 );
 
+//Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
 //Flash
 app.use(flash());
 
@@ -38,6 +45,7 @@ app.use(flash());
 app.use((req, res, next) => {
   res.locals.success_msg = req.flash("success_msg");
   res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
   next();
 });
 
--- PassportLogin/routes/users.js
+++ PassportLogin/routes/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const passport = require("passport");
 
 router.get("/login", (req, res) => {
   res.render("login");
@@ -57,9 +58,17 @@ router.post("/register", (req, res) => {
   }
 });
 
-router.post("/login", (req, res) => {
-  const {email, password} = req.body;
-  User.res.send("Post Login");
+router.post("/login", (req, res, next) => {
+  passport.authenticate("local", {
+    successRedirect: "/dashboard",
+    failureRedirect: "/users/login",
+    failureFlash: true,
+  })(req, res, next);
+});
+
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/users/login");
 });
 
 module.exports = router;
