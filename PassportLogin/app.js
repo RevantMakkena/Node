3640ca81c5d297011a2136a7ed21a01565624fb9
Revanth-M <revanthkumar.makkena@gmail.com>
2020-06-30 09:21:49 -0500

    Added register save to mongo and redirect to login

--- PassportLogin/app.js
+++ PassportLogin/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
+const flash = require("connect-flash");
+const session = require("express-session");
 const port = process.env.npm_package_config_port || 4001;
 
 //DB config
@@ -19,6 +21,25 @@ app.set("view engine", "ejs");
 
 //Body Parser
 app.use(express.urlencoded({extended: false}));
+
+//Express session
+app.use(
+  session({
+    secret: "secretNode",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+//Flash
+app.use(flash());
+
+//Global Var
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  next();
+});
 
 //Routes
 app.use("/", require("./routes/index"));
--- PassportLogin/routes/users.js
+++ PassportLogin/routes/users.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 
 router.get("/login", (req, res) => {
   res.render("login");
@@ -10,8 +12,54 @@ router.get("/register", (req, res) => {
 });
 
 router.post("/register", (req, res) => {
-  console.log(req.body);
-  res.send("Post Register");
+  const {name, email, password, password2} = req.body;
+  let errors = [];
+  if (!name || !email || !password || !password2)
+    errors.push({msg: "Please fill all fields!!"});
+
+  if (password !== password2) errors.push("Passwords should match");
+
+  if (password.length < 6)
+    errors.push({
+      msg: "Password should be greater than 6 characters ",
+    });
+
+  if (errors.length > 0) {
+    res.render("register", {
+      errors,
+      email,
+      name,
+      password,
+      password2,
+    });
+  } else {
+    User.findOne({email: email}).then((user) => {
+      if (user) {
+        errors.push({msg: "Email already exists"});
+        res.send("User exists");
+      } else {
+        const newUser = new User({name, email, password});
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser
+              .save()
+              .then(() => {
+                req.flash("success_msg", "Registered!!");
+                res.render("login");
+              })
+              .catch((err) => console.log(err));
+          });
+        });
+      }
+    });
+  }
+});
+
+router.post("/login", (req, res) => {
+  const {email, password} = req.body;
+  User.res.send("Post Login");
 });
 
 module.exports = router;
